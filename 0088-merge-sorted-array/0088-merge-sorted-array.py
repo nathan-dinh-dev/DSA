b08e9ec8455b12da16f7a9cd7499d5448f77e8d6
Nathan Dinh <nathan.d.workplace@gmail.com>
2025-08-22 12:42:43 -0700

    Time: 0 ms (100%), Space: 17.8 MB (71.49%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,17 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        arr_length = m + n
-
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[m + n - 1] = nums1[m - 1]
                 m -= 1
             else:
-                nums1[m + n - 1] = nums2[n - 1]
+                nums1[m + n -1] = nums2[n - 1]
                 n -= 1
         
         if n > 0:
-            nums1[:n] = nums2[:n]        
+            nums1[:n] = nums2[:n]
+
         return nums1
         
