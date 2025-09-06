d47e77a49aa7c66f7b6bcf1f635d9b3f41e23562
Nathan Dinh <nathan.d.workplace@gmail.com>
2025-09-06 11:37:11 -0700

    Time: 0 ms (100%), Space: 19.1 MB (23.46%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       
+        hashMap = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] not in hashMap:
+                hashMap[nums[i]] = i
+            else:
+                return [hashMap[target - nums[i]], i]
+        
+        return []                
