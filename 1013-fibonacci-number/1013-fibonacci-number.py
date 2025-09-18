50664bef41855c1e4469813cebcb3b05698873ea
Nathan Dinh <nathan.d.workplace@gmail.com>
2025-09-18 00:41:03 -0700

    Time: 626 ms (5.03%), Space: 17.5 MB (89.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -7,8 +7,9 @@ class Solution:
         
         if n in memo:
             return memo[n]
-        
-        memo[n] = self.fib(n - 1) + self.fib(n - 2)
+
+        memo[n] = self.fib(n-1) + self.fib(n-2)
+
         return memo[n]
         
         
